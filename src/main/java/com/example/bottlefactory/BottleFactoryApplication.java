d549054b85a36b44eb876e65a8f0ba97b8f82946
NarendraCoforge <narendra.coforge@gmail.com>
2022-11-18 09:15:05 +0530

    Initial commit

---
+++ src/main/java/com/example/bottlefactory/BottleFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.bottlefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottleFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottleFactoryApplication.class, args);
+	}
+
+}
